0ff4c9af5dec61ac070a191c281c4dcd85bba33e
Lucas Duete <lm.duete@gmail.com>
2018-06-14 21:25:52 -0300

    Injetando Filtros na aplicação.

--- src/main/java/io/github/lucasduete/mobileHubApi/MyApplication.java
+++ src/main/java/io/github/lucasduete/mobileHubApi/MyApplication.java
@@ -1,5 +1,8 @@
 package io.github.lucasduete.mobileHubApi;
 
+import io.github.lucasduete.mobileHubApi.infraSecurity.filters.CORSFilter;
+import io.github.lucasduete.mobileHubApi.infraSecurity.filters.FilterSecurityAuthentication;
+import io.github.lucasduete.mobileHubApi.infraSecurity.filters.FilterSecurityAuthorization;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import javax.ws.rs.ApplicationPath;
@@ -8,7 +11,13 @@ import javax.ws.rs.ApplicationPath;
 public class MyApplication extends ResourceConfig {
 
     public MyApplication() {
+        //Scaneia Dinamicamente pelos Controllers (classes com @Path)
         packages("io.github.lucasduete.mobileHubApi.controllers");
+
+        //Injeta os Filters
+        register(CORSFilter.class);
+        register(FilterSecurityAuthentication.class);
+        register(FilterSecurityAuthorization.class);
     }
 
 }

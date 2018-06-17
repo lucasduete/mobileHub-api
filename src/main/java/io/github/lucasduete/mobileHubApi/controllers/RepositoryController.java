174e9dc73718b364204941156eed31023497b94c
Lucas Duete <lm.duete@gmail.com>
2018-06-17 20:59:01 -0300

    Implementando listagem de repositórios do usuário, implementando limitação da quantidade de respositórios da resposta.

--- src/main/java/io/github/lucasduete/mobileHubApi/controllers/RepositoryController.java
+++ src/main/java/io/github/lucasduete/mobileHubApi/controllers/RepositoryController.java
@@ -1,9 +1,11 @@
 package io.github.lucasduete.mobileHubApi.controllers;
 
-import javax.json.Json;
-import javax.json.JsonArray;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
+import io.github.lucasduete.mobileHubApi.infraSecurity.Security;
+import io.github.lucasduete.mobileHubApi.infraSecurity.TokenManagement;
+import org.glassfish.jersey.process.internal.RequestContext;
+
+import javax.json.*;
+import javax.json.stream.JsonCollectors;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -11,14 +13,46 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
 import java.io.StringReader;
+import java.util.stream.Stream;
 
 @Path("repositories")
 public class RepositoryController {
 
     private static final String URL_BASE = "https://api.github.com/";
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Security
+    public Response getMyRepos(@Context SecurityContext securityContext) {
+
+        Client client = ClientBuilder
+                .newBuilder()
+                .build();
+
+        WebTarget webTarget = client.target(URL_BASE)
+                .path("user")
+                .path("repos");
+
+        Response response = webTarget
+                .request()
+                .header("Accept", "application/json, application/vnd.github.v3+json")
+                .header("Authorization", String.format("bearer %s", TokenManagement.getToken(securityContext)))
+                .get();
+
+        String jsonString = response.readEntity(String.class);
+        JsonReader jsonReader = Json.createReader(new StringReader(jsonString));
+        JsonArray jsonArray = jsonReader.readArray();
+
+        System.out.printf("\n\n TAMANHO: " + jsonArray.size());
+
+        jsonArray = jsonArray.stream().limit(10).collect(JsonCollectors.toJsonArray());
+        return Response.ok(jsonArray).build();
+    }
 
     @GET
     @Path("search")
@@ -50,6 +84,9 @@ public class RepositoryController {
 
         System.out.printf("\n\n TAMANHO: " + jsonObject.getInt("total_count"));
 
-        return Response.ok(jsonObject).build();
+        JsonArray jsonArray = jsonObject.getJsonArray("items");
+        jsonArray = jsonArray.stream().limit(10).collect(JsonCollectors.toJsonArray());
+
+        return Response.ok(jsonArray).build();
     }
 }

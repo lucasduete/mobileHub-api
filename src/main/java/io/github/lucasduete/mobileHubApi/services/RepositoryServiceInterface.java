6b4f62d40a750b53fa261f448cd24d99b6c0b76a
Lucas Duete <lm.duete@gmail.com>
2018-06-22 23:04:48 -0300

    Definindo interface para Repositoryservice.

---
+++ src/main/java/io/github/lucasduete/mobileHubApi/services/RepositoryServiceInterface.java
@@ -0,0 +1,14 @@
+package io.github.lucasduete.mobileHubApi.services;
+
+import io.github.lucasduete.mobileHubApi.entities.Repository;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface RepositoryServiceInterface {
+
+    public List<Repository> getMyRepos(String token) throws IOException;
+    public List<Repository> getMyStars(String token) throws IOException;
+    public List<Repository> search(String keyword) throws IOException;
+
+}

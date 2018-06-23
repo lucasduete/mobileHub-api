8d37241332fbd583047c92349a3a1d7a9c06b48d
Lucas Duete <lm.duete@gmail.com>
2018-06-22 21:40:04 -0300

    Implemetando método que recupera as o perfil de um usuário dado seu username

--- src/main/java/io/github/lucasduete/mobileHubApi/controllers/UserController.java
+++ src/main/java/io/github/lucasduete/mobileHubApi/controllers/UserController.java
@@ -7,6 +7,7 @@ import io.github.lucasduete.mobileHubApi.services.UserServiceInterface;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -28,7 +29,25 @@ public class UserController {
         try {
             return Response.ok(
                     userService.getProfile(token)
-                ).build();
+            ).build();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GET
+    @Path("{username}")
+    @Security
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getUser(@PathParam("username") String username) {
+
+        UserServiceInterface userService = new UserService();
+
+        try {
+            return Response.ok(
+                    userService.getUser(username)
+            ).build();
         } catch (IOException ex) {
             ex.printStackTrace();
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
